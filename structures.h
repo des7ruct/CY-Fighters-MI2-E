b4cc8d1af88d64f4a5ea62c7bdb0b66a77ea0286
des7ruct <canaxel05@gmail.com>
2025-04-07 18:01:45 +0200

    Update structures.h

--- structures.h
+++ structures.h
@@ -1,9 +1,25 @@
+/* Effets */
+
+
+/* Compétences */
+
+
+/* Passifs */
+
+
 /* Structure Combattant */
 
 typedef struct {
   char* nom ;
   int pv_max ;
-  int pv ;
-
+  int pv_courants ;
+  int attaque ;
+  int defense ;
+  int agilite ;
+  int vitesse ;
+  Competences* competences ;
+  Effets* effets_positifs ;
+  Effets* effets_negatifs ;
+  
 } Combattant ;
 
