e233d41f52d449201444adf89adadfa46ef26b42
Craapul95 <167770049+Craapul95@users.noreply.github.com>
2025-04-30 18:00:55 +0200

    Ajout de la fonction sur l'esquive

--- dump.c
+++ dump.c
@@ -1,1 +1,21 @@
+//Fonction de réduction des dégats par l'esquive
+
+float DegatsEsquive(int x, int y){
+/* Calcul les dégats réduis par l'agilité. On calcul le quotient des
+deux esquives et le résultat nous donnera le montant de dégats esquivé.
+*/
+    if(x<0 || y<0){
+        exit(1);
+    }
+
+    float a=x;
+    float b=y;
+    float coeff=1-(a/b);
+    if (x>y){
+        coeff=0;
+    }
+    return coeff;
+
+}
+
 
