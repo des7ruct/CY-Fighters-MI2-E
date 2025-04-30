ca3ced81de16ceaeaee6fa47f14ddc67d2232c63
des7ruct <canaxel05@gmail.com>
2025-04-30 18:09:10 +0200

    Update dump.c

--- dump.c
+++ dump.c
@@ -19,3 +19,32 @@ deux esquives et le résultat nous donnera le montant de dégats esquivé.
 }
 
 
+
+int aleatoire(int minimum, int maximum) {
+  /*
+  
+  Fonction prenant un minimum et un maximum positifs et génére un nombre aléatoire entre les deux. 
+  
+  */
+  
+  int resultat ;
+  
+  if ((minimum >= maximum) || (maximum <= 0)) {
+    printf("Erreur dans la fonction aleatoire avec les nombres minimums et maximums.") ;
+    exit(1) ;
+  }
+  
+  resultat = minimum + (rand() % ((maximum - minimum) + 1)) ;
+  
+  if ((resultat < 0) || (resultat )  ) {
+  }
+  
+}
+
+
+
+int multiplicateur_critique()
+
+
+
+int multiplicateur_type()
