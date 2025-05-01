7f84b11b6ac961575d0c51d0b523e3b5f24fcc31
Craapul95 <167770049+Craapul95@users.noreply.github.com>
2025-05-01 14:16:18 +0200

    Ajout du début du menu, et de 2 fonctions pour vérifier les fgets reçu par l'utilisateur

--- dump.c
+++ dump.c
@@ -48,3 +48,149 @@ int multiplicateur_critique()
 
 
 int multiplicateur_type()
+
+
+
+
+
+
+
+//Constante utilisé pour les fgets//
+#define V 100
+
+int VerifNb(char* test){
+//Vérifie que chaque caractère est bien un chiffre//
+    for(int i=0; test[i]!='\0'; i++){
+        if(test[i]<'0' || test[i]>'9'){
+            return 0;
+        }
+    }
+    return(1);
+}
+
+
+int VerifScanf(char* test){
+    //Vérifie que la chaine reçu ne soit pas vide//
+    if (test==NULL){
+        printf("\033[H\033[2J");
+        printf("ERREUR, veuillez-recommencer\n");
+        return(-1);
+    }
+    else if(test!=NULL){
+        test[strcspn(test, "\n")]=0;
+    }
+    /*Après avoir vérifier que la chaine ne contiennent que des chiffres,
+    retourne la chaîne en tant que int*/
+    if(VerifNb(test)){
+        return(atoi(test));
+    }
+    else{
+        printf("\033[H\033[2J");
+        printf("ERREUR, veuillez-recommencer\n");
+        return(-1);
+    }
+    return(0);
+
+}
+
+int Credit(){
+//Affichage des crédits du jeu//
+    char tampon[100];
+
+    printf("\033[H\033[2J");
+    printf("Voici les credits du jeu : \n\n");
+    printf("Jeu : Cy-Fighters\n\n");
+    printf("Developpeurs : \n -CAN Axel\n -ROUSSAUX Zachary\n -FARSAN Youssef\n");
+    printf("Inspire de : Summoners wars, Pokemon et Hearthstone\n");
+
+    //Choix entre retourner au menu ou quittez le programme//
+    printf("\n\nSouhaitez-vous : \n1-Retourner au menu\n\nou\n\n2-Quitter le jeu ?\n\n");
+    fgets(tampon, V, stdin);
+    int n=VerifScanf(tampon);
+
+    if(n!=1 && n!=2){
+        printf("ERREUR, fermeture du programme\n\n");
+        exit(1);
+    }
+
+    else{
+        return(n);
+    }
+}
+
+void Menu(){
+    int n=0;
+    char tampon[100];
+    printf(" Choisissez une option en entrant le chiffre adequat\n\n 1-Jouer contre un autre joueur\n 2-Jouer contre l'ordinateur\n");
+    printf(" 3-Tutoriel\n 4-Credits\n 5-Quitter le jeu\n\n");
+    printf("Reponse :   ");
+
+    // récupère la chaîne avec un fgets pour faciliter la vérification//
+    fgets(tampon, V, stdin);
+    n=VerifScanf(tampon);
+
+    if(n==-1){
+        Menu();
+    }
+
+    switch (n){
+
+        case 1:
+
+            break;
+
+
+        case 2:
+
+            break;
+
+
+        case 3:
+
+            break;
+
+        
+        case 4:
+            /* 
+                Affiche les crédits et ferme le programme ou redirige 
+                au menu selon le choix de l'utilisateur    
+            */
+            n=Credit();
+            if(n==1){
+                printf("\033[H\033[2J");
+                Menu();
+            }
+            else if(n==2){
+                printf("Fermeture du programme");
+                exit(0);
+            }
+            else{
+                printf("ERREUR, fermeture du programme\n");
+                exit(1);
+            }
+            break;
+            
+
+        case 5:
+            // Ferme le programme//
+            printf("Merci d'avoir joue a notre jeu !");
+            exit(0);
+            break;
+
+
+        default :
+            printf("\033[H\033[2J");
+            printf("Choix invalide, veuillez recommencer\n\n");
+            Menu();
+            break;
+
+
+    }
+}
+
+
+int main(){
+
+Menu();
+return(0);
+}
