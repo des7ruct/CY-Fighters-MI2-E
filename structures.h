f07ed9183b977fd740c5a79de333f33f7bd56064
des7ruct <canaxel05@gmail.com>
2025-05-02 11:45:38 +0200

    Update structures.h

--- structures.h
+++ structures.h
@@ -1,50 +1,62 @@
-/* Valeur de chaque type assignée à son nom pour faciliter l'écriture de code */
+#ifndef STRUCTURES_H
+  #define STRUCTURES_H
 
-#define FEU 1
-#define PLANTE 2
-#define EAU 3
+  /* inclusions */
 
 
-/* Effets */
-typedef struct {
-  int num ;
-  int type ;
-  float coeff ;
-}Effet;
+  /* contenu */
+
+  /* Valeur de chaque type assignée à son nom pour faciliter l'écriture de code */
+
+  #define FEU 1
+  #define PLANTE 2
+  #define EAU 3
+
+
+  /* Effets */
+  typedef struct {
+    int num ;
+    int type ;
+    float coeff ;
+  }Effet;
  
 
-/* Compétences */
-typedef struct {
-  int id ;
-  char* nom ;
-  char* description ;
+  /* Compétences */
+  typedef struct {
+    int id ;
+    char* nom ;
+    char* description ;
 
-} Competence ;
+  } Competence ;
 
-/* Passifs */
-typedef struct {
-  int id ;
-  char* nom ;
-  char* description ;
+  /* Passifs */
+  typedef struct {
+    int id ;
+    char* nom ;
+    char* description ;
 
-/* Structure Combattant */
+  } Passif ;
 
-typedef struct {
-  char* nom ;
-  int type ;
-  int pv_max ;
-  int attaque ;
-  int defense ;
-  int agilite ;
-  int vitesse ;
-  Competence* competences ;
-  Passif passif ;
+  /* Structure Combattant */
 
-  /* Non Prédéfini */
-  int stamina ;
-  int pv_courants ;
-  Effets* effets_positifs ;
-  Effets* effets_negatifs ;
-} Combattant ;
+  typedef struct {
+    char* nom ;
+    int type ;
+    int pv_max ;
+    int attaque ;
+    int defense ;
+    int agilite ;
+    int vitesse ;
+    Competence* competences ;
+    Passif passif ;
 
-/* Equipe */
+    /* Non Prédéfini */
+    int stamina ;
+    int pv_courants ;
+    Effets* effets_positifs ;
+    Effets* effets_negatifs ;
+  } Combattant ;
+
+  /* Equipe */
+
+#endif
