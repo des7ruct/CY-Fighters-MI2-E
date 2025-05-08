72aaa61081deb1db987e586ef80e33755330b656
des7ruct <canaxel05@gmail.com>
2025-05-08 10:37:25 +0200

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -49,6 +49,32 @@ int VerifScanf(char* test){
 }
 
 
+int Credit(){
+//Affichage des crédits du jeu//
+    char tampon[100];
+
+    printf("\033[H\033[2J");
+    printf("Voici les credits du jeu : \n\n");
+    printf("Jeu : Cy-Fighters\n\n");
+    printf("Developpeurs : \n -CAN Axel\n -ROUSSAUX Zachary\n -FARSAN Youssef\n");
+    printf("Inspire de : Summoners wars, Pokemon et Hearthstone\n");
+
+    //Choix entre retourner au menu ou quittez le programme//
+    printf("\n\nSouhaitez-vous : \n1-Retourner au menu\n\nou\n\n2-Quitter le jeu ?\n\n");
+    fgets(tampon, V, stdin);
+    int n=VerifScanf(tampon);
+
+    if(n!=1 && n!=2){
+        printf("ERREUR, fermeture du programme\n\n");
+        exit(1);
+    }
+
+    else{
+        return(n);
+    }
+}
+
+
 //Fonction de réduction des dégats par l'esquive
 
 float DegatsEsquive(int x, int y){
