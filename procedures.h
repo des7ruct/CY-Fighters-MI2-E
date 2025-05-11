e7a0df5df5f7b3dd8584854f29b1ccf2aff6f467
Craapul95 <167770049+Craapul95@users.noreply.github.com>
2025-05-11 21:50:50 +0200

    Update procedures.h

--- procedures.h
+++ procedures.h
@@ -6,5 +6,12 @@
 
   /* déclarations */
 
+void Codex (Combattant* tab, int taille) ;
+void Tuto() ;
+void Menu() ;
+void Creation_Equipe(Combattant* tab, int nb_combattant_tot, int nb_combattant_eq, Equipe* A, Equipe* B) ;
+void Interface_Combat(Equipe* A, Equipe* B, Combattant Attaquant) ;
+void Combat(Combattant* tab, int n_tot, int n_eq) ;
+
 
 #endif
